2561ef34f7d1da17281ff491d66df64674e5af05
Jamal Natour <me@Jamals-MacBook-Pro.local>
2012-04-17 14:45:57 +0100

    BUGFIX: Version number was bumped without updating test, test is now updated

--- test/test_version.rb
+++ test/test_version.rb
@@ -2,6 +2,6 @@ require File.expand_path('../test_helper.rb',__FILE__)
 
 class TestVersion < Test::Unit::TestCase
   def test_version_number
-    assert_equal '0.0.1', CapyBrowser::VERSION
+    assert_equal '0.0.2', CapyBrowser::VERSION
   end
 end
